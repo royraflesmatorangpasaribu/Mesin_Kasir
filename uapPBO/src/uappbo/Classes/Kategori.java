424cbedc396345a0c918201503145f4e7b1d89bd
MRadityaAdhirajasa <92293185+MRadityaAdhirajasa@users.noreply.github.com>
2022-12-18 23:08:31 +0700

    Add files via upload

---
+++ uapPBO/src/uappbo/Classes/Kategori.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uappbo.Classes;
+
+public class Kategori {
+    private String nama_kategori;
+
+    public String getNama_kategori() {
+        return nama_kategori;
+    }
+
+    public void setNama_kategori(String nama_kategori) {
+        this.nama_kategori = nama_kategori;
+    }
+    
+    public void getProduk(){
+        
+    }
+}
