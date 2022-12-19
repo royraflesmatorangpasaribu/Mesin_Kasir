0739bef1d551ee1846aef265ff19d20a26566238
MRadityaAdhirajasa <92293185+MRadityaAdhirajasa@users.noreply.github.com>
2022-12-19 12:12:50 +0700

    Update StrukController.java

--- uapPBO/src/uappbo/StrukController.java
+++ uapPBO/src/uappbo/StrukController.java
@@ -1,24 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package uappbo;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
-/**
- * FXML Controller class
- *
- * @author egyve
- */
 public class StrukController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    
+      @FXML
+    private Button Menu;
+
+    @FXML
+    void OpenForm(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Menu.fxml"));
+          
+        Stage stage = (Stage) Menu.getScene().getWindow();
+        stage.setScene(new Scene(root));
+    }
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
