ab0607d8221ae2f8b58f6a4bd801ae151b310256
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2022-12-19 12:01:21 +0700

    Update BarangListController.java

--- uapPBO/src/uappbo/BarangListController.java
+++ uapPBO/src/uappbo/BarangListController.java
@@ -16,10 +16,12 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import javax.swing.JOptionPane;
 import jdbc.BarangModel;
 import jdbc.MakananModel;
 import uappbo.Classes.Barang;
 import uappbo.Classes.Makanan;
+
 
 /**
  * FXML Controller class
@@ -48,35 +50,15 @@ public class BarangListController implements Initializable {
     @FXML
     private Button addBtn;
 
-    @FXML
-    private Button delBtn;
 
     @FXML
     private Button mknBack;
 
-    @FXML
-    private Button mknbuy;
+ 
     
     @FXML
     private Button showView;
     
-    @FXML
-    private TableView<Barang> tabelBarang;
-
-    @FXML
-    private TableColumn<Barang, Double> tblDiskon;
-
-    @FXML
-    private TableColumn<Barang, String> tblBarcode;
-
-    @FXML
-    private TableColumn<Barang, Double> tblHarga;
-
-    @FXML
-    private TableColumn<Barang, String> tblNama;
-
-    @FXML
-    private TableColumn<Barang, Integer> tblStok;
     
     @FXML
     void openForm(ActionEvent event) throws IOException {
@@ -94,13 +76,7 @@ public class BarangListController implements Initializable {
         stage.setScene(new Scene(root));
     }
 
-    @FXML
-    void ToStruk(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("Struk.fxml"));
-          
-        Stage stage = (Stage) mknbuy.getScene().getWindow();
-        stage.setScene(new Scene(root));
-    }
+
 
     @FXML
     void addData(ActionEvent event) {
@@ -110,33 +86,15 @@ public class BarangListController implements Initializable {
         double disc = Double.parseDouble(Diskon.getText());
                 
         Barang brg = new Barang(Nama.getText(), hrg, jml, disc, Barcode.getText(), Expired.getText());
-        ObservableList<Barang> brg1 = tabelBarang.getItems();
-        brg1.add(brg);
-        tabelBarang.setItems(brg1);
         model.addBarangSQL(brg);
         System.out.println(Nama.getText());
+        JOptionPane.showMessageDialog(null, "Data Berhasil Di Tambahkan!!!");
+        
     }
 
-    @FXML
-    void removeData(ActionEvent event) {
-        int selectedID = tabelBarang.getSelectionModel().getSelectedIndex();
-        tabelBarang.getItems().remove(selectedID);
-        BarangModel model = new BarangModel();
-        double hrg = Double.parseDouble(Harga.getText());
-        int jml = Integer.parseInt(Jumlah.getText());
-        double disc = Double.parseDouble(Diskon.getText());
-                
-        Barang brg = new Barang(Nama.getText(), hrg, jml, disc, Barcode.getText(), Expired.getText());
-        model.deleteBarangSQL(brg);
-    }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        tblNama.setCellValueFactory(new PropertyValueFactory<>("Nama"));
-        tblHarga.setCellValueFactory(new PropertyValueFactory<>("Harga"));
-        tblStok.setCellValueFactory(new PropertyValueFactory<>("Jumlah"));
-        tblDiskon.setCellValueFactory(new PropertyValueFactory<>("Diskon"));
-        tblBarcode.setCellValueFactory(new PropertyValueFactory<>("Barcode"));
     }    
     
 }
